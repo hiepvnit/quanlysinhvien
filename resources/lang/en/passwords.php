a3120d7b87182f19e3eff4db4bd0d1b810d0ee37
hiepvnit <hiepvn.it@gmail.com>
2017-08-03 21:29:51 +0700

    translate msg to vietnamese

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Mật khẩu phải ít nhất 6 kí tự và phải trùng khớp.',
+    'reset' => 'Mật khẩu của bạn đã đổi',
+    'sent' => 'Link để reset mật khẩu đã được gửi qua mail!',
+    'token' => 'Mã kích hoạt tài khoản đã hết hạn.',
+    'user' => "Không tồn tại tài khoản.",
 
 ];
